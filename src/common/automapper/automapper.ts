bc796e88cd757d8d91256270d462ccb124431bfe
Getulio <getuliomagela@outlook.com>
2023-08-30 21:28:34 -0300

    uses abstract class for abstraction

--- src/common/automapper/automapper.ts
+++ src/common/automapper/automapper.ts
@@ -2,11 +2,13 @@ import { Mapper } from '@automapper/core';
 import { InjectMapper } from '@automapper/nestjs';
 import { Type } from '@nestjs/common';
 
-export interface IAutomapper {
-  map<T, K>(source: T, SourceClass: Type<T>, DestinationClass: Type<K>): K;
+export abstract class IAutomapper {
+  abstract map<T, K>(
+    source: T,
+    SourceClass: Type<T>,
+    DestinationClass: Type<K>,
+  );
 }
-
-export abstract class IAutomapper implements IAutomapper {}
 
 export class Automapper implements IAutomapper {
   constructor(@InjectMapper() private readonly mapper: Mapper) {}
