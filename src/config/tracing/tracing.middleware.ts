51d7d0fb618d62de48cecf5928720f4467a76f49
Getulio <getuliomagela@outlook.com>
2023-08-30 21:28:43 -0300

    simplifies tracer middleware

--- src/config/tracing/tracing.middleware.ts
+++ src/config/tracing/tracing.middleware.ts
@@ -8,45 +8,12 @@ export class TracingMiddleware implements NestMiddleware {
   constructor(private readonly context: TracingContext) {}
 
   use(req: Request, _res: Response, next: NextFunction) {
-    /**
-     * @TODO @GG
-     * In the future, allow passing these as a config
-     */
-    const traceId =
-      req.get('x-trace-id') ??
-      req.body?.message?.attributes?.traceId ??
-      randomUUID();
-
-    const { clientId, clientName } = this.getClient(req);
+    const traceId = req.get('x-trace-id') ?? randomUUID();
 
     req['traceId'] = traceId;
 
     const context = new Map();
     context.set('traceId', traceId);
-    context.set('clientId', clientId);
-    context.set('clientName', clientName);
-
     this.context.run(context, async () => next());
-  }
-
-  private getClient(req: Request) {
-    const authorization =
-      req.get('X-Forwarded-Authorization') ?? req.get('Authorization');
-
-    if (!authorization) {
-      return {};
-    }
-
-    const tryParse = () => {
-      try {
-        const tokenBody = authorization.split('.').at(1);
-        return JSON.parse(Buffer.from(tokenBody, 'base64').toString('utf8'));
-      } catch {
-        return authorization;
-      }
-    };
-
-    req['auth'] = tryParse();
-    return req['auth'];
   }
 }
